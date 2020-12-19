5fecc87f271b24d2c03b510286d2453c19651f75
Silvia Criado <silvie.criado@gmail.com>
2020-12-19 01:28:57 +0100

    convert json data into html: to-do-list items

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-import database from 'db.json';
+class ToDoList {
+    db = {
+        "items": [
+            {
+                "title": "hacer la compra", 
+                "done": true,
+                "date": "19/12/2021"
+            },
+            {
+                "title": "sacar al perro", 
+                "done": false,
+                "date": "21/12/2021"
+            }
+        ]
+    };
+
+    printItems() {
+        const htmlListElements = this.db.items.map(
+            (element) => {
+                return `<li>${element.title}</li>`;
+            }
+        )
+        const list = document.getElementById('to-dos');
+        list.innerHTML = htmlListElements;
+    }
+}
+
+const toDoList = new ToDoList();
+toDoList.printItems();
