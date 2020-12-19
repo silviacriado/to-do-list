da1819fcb1a75afe7e277e14c5e80f3cb6e2d595
Silvia Criado <silvie.criado@gmail.com>
2020-12-19 12:55:19 +0100

    created a form to insert new items in db

--- app.js
+++ app.js
@@ -1,27 +1,55 @@
 class ToDoList {
-    db = {
-        "items": [
-            {
-                "title": "hacer la compra", 
-                "done": true,
-                "date": "19/12/2021"
-            },
-            {
-                "title": "sacar al perro", 
-                "done": false,
-                "date": "21/12/2021"
-            }
-        ]
-    };
+   
+    database;
+
+    constructor() {
+        this.database = {
+            "items": [
+                {
+                    "title": "hacer la compra", 
+                    "done": true,
+                    "date": "19/12/2021"
+                },
+                {
+                    "title": "sacar al perro", 
+                    "done": false,
+                    "date": "21/12/2021"
+                }
+            ]
+        };
+        document.getElementById('form-insert').addEventListener('submit', this.insertItem.bind(this));
+    }
+    
 
     printItems() {
-        const htmlListElements = this.db.items.map(
+        const htmlListElements = this.database.items.map(
             (element) => {
                 return `<li>${element.title}</li>`;
             }
         )
         const list = document.getElementById('to-dos');
         list.innerHTML = htmlListElements;
+    }
+
+    insertItem(event) {
+        event.preventDefault();
+        debugger;
+        const newItem = {
+            title: event.srcElement[0].value,
+            done: false,
+            date: this.getTodayDate()
+        };
+        this.database.items.push(newItem);
+        this.printItems();
+    }
+
+    getTodayDate() {
+        const todayDate = new Date();
+        const day = todayDate.getDay();
+        const month = todayDate.getMonth() +1;
+        const year = todayDate.getFullYear();
+
+        return `${day}/${month}/${year}`;
     }
 }
 
